5b611a7fe748174cfe9a12ca787f403466e0a040
André Pinheiro <drepimelo@hotmail.com>
2025-07-24 20:40:08 -0300

    Comentários removidos

--- js/index.js
+++ js/index.js
@@ -7,50 +7,43 @@ const smoke = document.querySelector('#smoke');
 
 window.addEventListener('scroll', () => {
   const scrollY = window.scrollY;
-  const maxScroll = 800; // Ajuste conforme necessário
-  const progress = Math.min(scrollY / maxScroll, 1); // valor de 0 a 1
+  const maxScroll = 800; 
+  const progress = Math.min(scrollY / maxScroll, 1); 
 
-  const deslocamento = -15 + (progress * 15); // vai de -100% até 0%
+  const deslocamento = -15 + (progress * 15); 
   smoke.style.transform = `translateX(${deslocamento}%)`;
 });
 
-
-// Pega todos os elementos do menu (inclusive o da logo)
 const headerContents = document.querySelectorAll('.header--wrapper_content');
 
-// Pega especificamente a logo
 const logo = document.querySelector('.header--logo');
 
-// Define a altura da hero para usar como limite do scroll
 const heroHeight = document.querySelector('#hero').offsetHeight;
 
 window.addEventListener('scroll', () => {
   
   if (window.scrollY > heroHeight - 50) {
-    // Quando sair da hero, diminui a logo e mostra os outros links
+
     logo.style.transform = 'scale(0.7)';
     
     headerContents.forEach(el => {
-      el.style.opacity = '1'; // Todos os itens da navbar aparecem
+      el.style.opacity = '1';
     });
 
   } else {
-    // Quando estiver na hero, logo grande e links invisíveis
+
     logo.style.transform = 'scale(1)';
     
     headerContents.forEach(el => {
       if (el.contains(logo)) {
-        el.style.opacity = '1'; // Deixa a logo visível sempre
+        el.style.opacity = '1'; 
       } else {
-        el.style.opacity = '0'; // Esconde os outros links
+        el.style.opacity = '0'; 
       }
     });
   }
 });
 
-// No index.js
-
-// Selecione todos os elementos de vídeo que você deseja observar
 const videos = document.querySelectorAll('.main--content video');
 
 videos.forEach((video) => {
@@ -58,10 +51,10 @@ videos.forEach((video) => {
     (entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          video.currentTime = 0; // Reinicia o vídeo
+          video.currentTime = 0; 
           video.play();
         } else {
-          video.pause(); // Opcional: pausar o vídeo quando não estiver visível
+          video.pause(); 
         }
       });
     },
